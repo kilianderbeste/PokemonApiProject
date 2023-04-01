3f70e464cdda8e67164fe4a603f77ef1c2e3f167
Othman Adi <adiatwork@outlook.com>
2023-04-01 12:15:43 +0200

    * 🎨 style(index.html): reformat code and add Bootstrap grid classes to input and button elements * 🚀 chore(index.html): add class names to HTML elements for easier styling in CSS * 🐛 fix(index.html): add missing newline at end of file
    * 🎨 style(style.css): add custom CSS variables and classes for card styling
    * ✨ feat(pokepoke.js): add functionality to fetch and display pokemon data based on user input

--- pokepoke.js
+++ pokepoke.js
@@ -1,1 +1,26 @@
-const apiEndPunkt = "https://pokeapi.co/api/v2/pokemon";
+const cardImage = document.querySelector(".card-image-top");
+
+const pokeName = document.querySelector(".pokeName");
+
+const searchPoke = document.querySelector(".pokeSearch");
+
+function fetchpoke() {
+
+    let valueOfSearch = searchPoke.value;
+
+    const apiEndPunkt = `https://pokeapi.co/api/v2/pokemon/${valueOfSearch}`;
+
+
+    // alert(valueOfSearch);
+
+    fetch(apiEndPunkt)
+        .then((response) => response.json())
+        .then((data) => {
+
+            cardImage.src = data.sprites.front_default;
+
+            pokeName.textContent = data.name;
+
+        })
+        .catch((error) => { alert("Pokemon not found or Network down", error) })
+}
